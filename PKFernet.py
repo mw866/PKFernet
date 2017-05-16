c31337daf36f3691ce327d640cc7ca81765d146a
Chris M Wang <mw866@cornell.edu>
2017-05-15 22:08:51 -0400

    Implemented encrypt()

--- PKFernet.py
+++ PKFernet.py
@@ -10,25 +10,21 @@ import six
 
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes, padding, serialization
+from cryptography.hazmat.primitives import hashes, serialization, ciphers, hmac
+from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.hmac import HMAC
 
 import json
 
 class PKFernet:
-    def __init__(self, priv_keyring, public_keyrings):
+    def __init__(self, sender_priv_keyring, receiver_public_keyrings):
         ''' loads all private keys and public keys in the keyrings. Keyring is a proxy for real key management system, and it is simply a json blob containing keys in PEM format. 
         @priv_keyring contains all the private keys in the system
-        @public_keyrings contains all the public keys of the friends with whom you want to share messages. The @public_keyrings should look something similar to the following,
-        {
-            receiver1_name: receiver1_public_keyring,
-            receiver2_name: receiver2_public_keyring,
-            ...
-        }'''
+        @public_keyrings contains all the public keys of the friends with whom you want to share messages. '''
         # TODO
-        self.priv_keyring = priv_keyring
-        self.public_keyrings = public_keyrings
+        self.sender_priv_keyring = sender_priv_keyring
+        self.receiver_public_keyrings = receiver_public_keyrings
 
     def encrypt(self, msg, receiver_name, receiver_enc_pub_key_alias, sender_sign_header, adata='', sign_also=True):
         '''Encrypt the message msg, using the receiver's encryption_pub_key, and generate signature using sender's sender_sign_priv_key. You have to obtain the keys from the private and public key_rings that you passed during creating this PKFernet object. 
@@ -42,11 +38,8 @@ class PKFernet:
         # Encryption Algorithm:  the concatenation of the first 3 parts of the receiver_enc_pub_key_alias
         enc_algorithm = '.'.join(receiver_enc_pub_key_alias.split('.')[:2])
 
-
-        # RKsym:  a URL safe base64 encoded string which Fernet2 will use to derive keys. For ECC based keys, this is ephemeral symmetric key which is generated using receiver's public key (Bpk.enc) and sender's ephemeral private key (Rsk). Use cryptography.io function to generate this key. If you are using RSA, then it is a random string of length  at least 32-bytes.
-        RKsym = base64.urlsafe_b64encode(os.urandom(32))
-
-        receiver_public_keys = self.import_pub_keys('receiver', self.public_keyrings)
+        # Loading receiver public key rings
+        receiver_public_keys = self.import_pub_keys('receiver', self.receiver_public_keyrings)
 
         # Receiver's Encryption Public Key
         receiver_enc_pub_key_string = self.deserialize(receiver_public_keys['rsa.2048.1.enc.pub'])
@@ -56,21 +49,51 @@ class PKFernet:
         receiver_sig_pub_key_string = self.deserialize(receiver_public_keys['rsa.2048.1.sig.pub'])
         receiver_sig_pub_key = serialization.load_pem_public_key(bytes(receiver_sig_pub_key_string, 'utf-8'), backend=default_backend())
 
+        # Loading sender private key ring
+        sender_private_key_ring = self.import_priv_key(self.sender_priv_keyring)
+        #  Sender's private signing key
+        sender_sig_priv_key_string = self.deserialize(sender_private_key_ring['rsa.2048.1.sig.priv'])
+        sender_sig_priv_key = serialization.load_pem_private_key(bytes(sender_sig_priv_key_string, 'utf-8'), password=None, backend=default_backend())
 
+        #  Signing the message
+        signer = sender_sig_priv_key.signer(
+            padding.PSS(
+                mgf = padding.MGF1(hashes.SHA256()),
+                salt_length = padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        signer.update(bytes(msg,'utf-8'))
+        signature = signer.finalize()
+        signature_b64 = base64.urlsafe_b64encode(b'rsa_with_sha256.2048.1' + signature)
 
+        # Symmetrically encrypting message
+        R_sym = os.urandom(32)
+        iv = b'\x00'*16  # IV is 256-bit for AES256;
+        sym_encryptor = ciphers.Cipher(ciphers.algorithms.AES(R_sym), ciphers.modes.CTR(iv), backend=default_backend()).encryptor()
+        msg_encrypted = sym_encryptor.update(bytes(msg, 'utf-8')) + sym_encryptor.finalize()
+        msg_encrypted_b64 = base64.urlsafe_b64encode(msg_encrypted)
 
-        # <Rpk>  - This field is used to generate the symmetric key for encryption asymmetrically between the sender and receiver.
+        # Create some adata e.g.Timestamp, 64 bits; ">Q": big-endian unsigned long long integer of 8 bytes
+        adata = struct.pack(">Q", int(time.time()))
+        adata_b64 = base64.urlsafe_b64encode(msg_encrypted)
 
-        # Rpk = receiver_enc_pub_key.encrypt(
-        #     RKsym,
-        #     padding.OAEP(
-        #       mgf=padding.MGF1(algorithm=hashes.SHA1()),
-        #       algorithm = hashes.SHA1()
-        #     )
-        # )
+        # Generating HMAC
+        R_hmac = os.urandom(32)
+        h = hmac.HMAC(R_hmac, hashes.SHA256(), backend=default_backend())
+        h.update(adata_b64 + b'|' + signature_b64 + b'|' + msg_encrypted_b64)
+        hmac_b64 = base64.urlsafe_b64encode(h.finalize())
 
-        # Ciphertext format: <adata> | <Enc_Algorithm> | <Rpk> | <Fernet2_ciphertext>
-        # ctx = '|'.join([adata, enc_algorithm, Rpk, Fernet2_ciphertext])
+        # Assymetrically encrypt R = (R_sym || R_hmac)
+        R_encrypted = receiver_enc_pub_key.encrypt(
+            R_sym + R_hmac,
+            padding.OAEP(
+                mgf = padding.MGF1(algorithm=hashes.SHA1()), algorithm = hashes.SHA1(), label = None)
+        )
+        R_encrypted_b64 = base64.urlsafe_b64encode(R_encrypted)
+
+        # Ciphertext format: <adata_b64>
+        ctx = b'|'.join([adata_b64, b'rsa.2048.1', R_encrypted_b64, msg_encrypted_b64, signature_b64, hmac_b64])
         return ctx
 
     def decrypt(self, ctx, sender_name, verfiy_also=True):
@@ -87,14 +110,13 @@ class PKFernet:
         '''import public keys of the receiver into its @public_keyrings, public_keyring, should be of a json dictionary containing keyrings indexed by the receiver's name.'''
         with open(receiver_public_keyring, 'r', encoding='utf-8') as f:
             public_keys = json.load(f)[receiver_name]
-            # public_pem_data = self.deserialize(json.load(f)[receiver_name])
-            # public_key = serialization.load_pem_public_key(public_pem_data, backend=default_backend())
             return public_keys
 
     def import_priv_key(self, sender_priv_keyring):
         '''import private key of sender'''
         with open(sender_priv_keyring, 'r', encoding='utf-8') as f:
-            return json.load(f)
+            private_keys = json.load(f)
+            return private_keys
 
     def deserialize(self, pem_text):
         ''' deserialize the key value in the json keystrings '''
@@ -104,13 +126,10 @@ class PKFernet:
         pem_text_deserialized = pem_text_list[0] +'\n'+ pem_payload_text +'\n'+pem_text_list[-2]
         return pem_text_deserialized
 
-class Fernet2:
-    # TODO Pending clarification
-    pass
 
 if __name__ == '__main__':
     msg = "this is a test message"
-    pf = PKFernet(priv_keyring = 'sender/sender_priv_keyring.json', public_keyrings='receiver/receiver_pub_keyrings.json')
+    pf = PKFernet(sender_priv_keyring='sender/sender_priv_keyring.json', receiver_public_keyrings='receiver/receiver_pub_keyrings.json')
     c = pf.encrypt(msg, receiver_name='receiver', receiver_enc_pub_key_alias= 'rsa.2048.1.enc.priv', sender_sign_header='rsa.2048.1.sig.priv', adata='', sign_also = True)
     # m = pf.decrypt(ctx, sender_name, verfiy_also=True)
 
