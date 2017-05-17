915db05e5c26a540e2775df6f445b46243729f43
Chris M Wang <mw866@cornell.edu>
2017-05-17 19:05:12 -0400

    Adjust the unit tests for cross-testing

--- PKFernet.py
+++ PKFernet.py
@@ -235,20 +235,20 @@ class TestPKFernet(object):
         assert sender_pub_key_json_export.replace(' ', '') in sender_pub_key_json_file.replace(' ', '')
 
     def test_cross_decryption(self):
-        "Cross test with brook's ciphertext and public key"
-
+        "Cross-testing decryption of brook's ciphertext and public key"
         with open('brook/ciphertext.txt', 'r', encoding='utf-8') as f:
             brook_ciphertext = f.read()
-
         brook_pf = PKFernet(priv_keyring='sender/sender_priv_keyring.json', public_keyrings='brook/public_key.json')
-        m = brook_pf.decrypt(brook_ciphertext, sender_name='brook', verfiy_also=True)
-        print(m)
+        msg = brook_pf.decrypt(brook_ciphertext, sender_name='brook', verfiy_also=True)
+        assert msg == b'This is a simple message'
 
     def test_cross_encryption(self):
+        """Encrypting ciphtertext for cross testing by brook"""
         msg = b'this is a test message'
         sender_pf = PKFernet(priv_keyring='sender/sender_priv_keyring.json', public_keyrings='brook/public_key.json')
         ctx = sender_pf.encrypt(msg, receiver_name='brook', receiver_enc_pub_key_alias='rsa.2048.1.enc.priv', sender_sign_header='rsa_with_sha256.2048.1', adata='', sign_also=True)
         print(str(ctx, 'utf-8'))
+        assert True
 
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
