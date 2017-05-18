dd3a6674ae2605ef35f74535abc41ef171a6b26f
Chris M Wang <mw866@cornell.edu>
2017-05-17 21:08:11 -0400

    Update the private key to the Backdoor Implementor (Brook) in the pytest.

--- PKFernet_backdoor.py
+++ PKFernet_backdoor.py
@@ -128,7 +128,6 @@ class PKFernetBackdoor:
         if b'rsa_with_sha256.2048.1' not in base64.urlsafe_b64decode(sig_algorithm_b64):
             raise UnsupportedAlgorithm
 
-
         # [BACKDOOR] Decrypting the message via the Rsym ecnrypted
         if backdoor_mode:
             # BACKDOOR here: extract and decrypt {Rsys} in the adata
@@ -260,15 +259,16 @@ class TestPKFernetBackdoor(object):
     """pytest class for PKFernet"""
 
     def test_backdoor(self):
-        """Test basic ecnryption and decryption"""
-        # Sender sends the message
+        """Test  encryption and decryption in the backdoor mode"""
+        # Sender encrypts and sends the message in the backdoor mode
         msg = b'this is a test message'
         sender_pf = PKFernetBackdoor(priv_keyring='sender/sender_priv_keyring.json', public_keyrings='receiver/receiver_pub_keyrings.json')
         ctx = sender_pf.encrypt(msg, receiver_name='receiver', receiver_enc_pub_key_alias='rsa.2048.1.enc.priv', sender_sign_header='rsa_with_sha256.2048.1', adata='', sign_also=True, backdoor_mode=True)
 
-        # Receiver receives the message Note: the priv_keying is INCORRECT and not from the receiver
-        receiver_pf = PKFernetBackdoor(priv_keyring='sender/sender_priv_keyring.json', public_keyrings='sender/sender_pub_keyrings.json')
-        m = receiver_pf.decrypt(ctx, sender_name='sender', verfiy_also=True, backdoor_mode=True)
+        # The backdoor implementor (Brook) intercepts and decrypts the message in the backdoor mode
+        # The `priv_keying` is not from the correct receiver and can be anyone's private key
+        brook_pf = PKFernetBackdoor(priv_keyring='brook/private_key.json', public_keyrings='sender/sender_pub_keyrings.json')
+        m = brook_pf.decrypt(ctx, sender_name='sender', verfiy_also=True, backdoor_mode=True)
 
         assert (msg == m)
 
