f8b0b018b3a7d3a26e7484e13d7ea19fd98e8f0f
Chris M Wang <mw866@cornell.edu>
2017-05-17 21:18:02 -0400

    Spin off the pytest scrypt from PKFernet.py into test_PKFernet.py

--- PKFernet.py
+++ PKFernet.py
@@ -206,49 +206,4 @@ class PKFernet:
         return pem_text_serialized
 
 
-class TestPKFernet(object):
-    """pytest class for PKFernet"""
-
-    def test_basic(self):
-        """Test basic ecnryption and decryption"""
-        # Sender sends the message
-        msg = b'this is a test message'
-        sender_pf = PKFernet(priv_keyring='sender/sender_priv_keyring.json', public_keyrings='receiver/receiver_pub_keyrings.json')
-        ctx = sender_pf.encrypt(msg, receiver_name='receiver', receiver_enc_pub_key_alias='rsa.2048.1.enc.priv', sender_sign_header='rsa_with_sha256.2048.1', adata='', sign_also=True)
-        print(str(ctx, 'utf-8'))
-        # Receiver receives the message
-        receiver_pf = PKFernet(priv_keyring='receiver/receiver_priv_keyring.json', public_keyrings='sender/sender_pub_keyrings.json')
-        m = receiver_pf.decrypt(ctx, sender_name='sender', verfiy_also=True)
-
-        assert (msg == m)
-
-    def test_export_pub_keys(self):
-        """Test export public keys in JSON"""
-        # Exporting sender public keys from private keys
-        sender_pf = PKFernet(priv_keyring='sender/sender_priv_keyring.json', public_keyrings='receiver/receiver_pub_keyrings.json')
-        sender_pub_key_json_export = sender_pf.export_pub_keys(key_alias_list=[])
-
-        # Loading sender existing public keyring file
-        with open('sender/sender_pub_keyrings.json', 'r', encoding='utf-8') as f:
-            sender_pub_key_json_file = f.read()
-
-        assert sender_pub_key_json_export.replace(' ', '') in sender_pub_key_json_file.replace(' ', '')
-
-    def test_cross_decryption(self):
-        "Cross-testing decryption of brook's ciphertext and public key"
-        with open('brook/ciphertext.txt', 'r', encoding='utf-8') as f:
-            brook_ciphertext = f.read()
-        brook_pf = PKFernet(priv_keyring='sender/sender_priv_keyring.json', public_keyrings='brook/public_key.json')
-        msg = brook_pf.decrypt(brook_ciphertext, sender_name='brook', verfiy_also=True)
-        assert msg == b'This is a simple message'
-
-    def test_cross_encryption(self):
-        """Encrypting ciphtertext for cross testing by brook"""
-        msg = b'this is a test message'
-        sender_pf = PKFernet(priv_keyring='sender/sender_priv_keyring.json', public_keyrings='brook/public_key.json')
-        ctx = sender_pf.encrypt(msg, receiver_name='brook', receiver_enc_pub_key_alias='rsa.2048.1.enc.priv', sender_sign_header='rsa_with_sha256.2048.1', adata='', sign_also=True)
-        print(str(ctx, 'utf-8'))
-        assert True
-
-
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
---
+++ test_PKFernet.py
@@ -0,0 +1,42 @@
+from PKFernet import PKFernet
+
+def test_loopback():
+    """Test basic ecnryption and decryption"""
+    # Sender sends the message
+    msg = b'this is a test message'
+    sender_pf = PKFernet(priv_keyring='sender/sender_priv_keyring.json', public_keyrings='receiver/receiver_pub_keyrings.json')
+    ctx = sender_pf.encrypt(msg, receiver_name='receiver', receiver_enc_pub_key_alias='rsa.2048.1.enc.priv', sender_sign_header='rsa_with_sha256.2048.1', adata='', sign_also=True)
+    print(str(ctx, 'utf-8'))
+    # Receiver receives the message
+    receiver_pf = PKFernet(priv_keyring='receiver/receiver_priv_keyring.json', public_keyrings='sender/sender_pub_keyrings.json')
+    m = receiver_pf.decrypt(ctx, sender_name='sender', verfiy_also=True)
+
+    assert (msg == m)
+
+def test_export_pub_keys():
+    """Test export public keys in JSON"""
+    # Exporting sender public keys from private keys
+    sender_pf = PKFernet(priv_keyring='sender/sender_priv_keyring.json', public_keyrings='receiver/receiver_pub_keyrings.json')
+    sender_pub_key_json_export = sender_pf.export_pub_keys(key_alias_list=[])
+
+    # Loading sender existing public keyring file
+    with open('sender/sender_pub_keyrings.json', 'r', encoding='utf-8') as f:
+        sender_pub_key_json_file = f.read()
+
+    assert sender_pub_key_json_export.replace(' ', '') in sender_pub_key_json_file.replace(' ', '')
+
+def test_cross_decryption():
+    "Cross-testing decryption of brook's ciphertext and public key"
+    with open('brook/ciphertext.txt', 'r', encoding='utf-8') as f:
+        brook_ciphertext = f.read()
+    brook_pf = PKFernet(priv_keyring='sender/sender_priv_keyring.json', public_keyrings='brook/public_key.json')
+    msg = brook_pf.decrypt(brook_ciphertext, sender_name='brook', verfiy_also=True)
+    assert msg == b'This is a simple message'
+
+def test_cross_encryption():
+    """Encrypting ciphtertext for cross testing by brook"""
+    msg = b'this is a test message'
+    sender_pf = PKFernet(priv_keyring='sender/sender_priv_keyring.json', public_keyrings='brook/public_key.json')
+    ctx = sender_pf.encrypt(msg, receiver_name='brook', receiver_enc_pub_key_alias='rsa.2048.1.enc.priv', sender_sign_header='rsa_with_sha256.2048.1', adata='', sign_also=True)
+    print(str(ctx, 'utf-8'))
+    assert True
